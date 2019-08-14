7dfe2cb1a0b06cb5e417157620d5946c8783d6f3
morwen80 <ribesnero80@gmail.com>
2019-08-14 17:02:02 +0200

    search section bootstrap implemented

--- src/components/RecipeSearch.js
+++ src/components/RecipeSearch.js
@@ -3,7 +3,6 @@ import React from 'react';
 class RecipeSearch extends React.Component {
   constructor(props){
     super(props)
-
     this.state = {
       term: ''
     }
@@ -11,16 +10,23 @@ class RecipeSearch extends React.Component {
 
   render(){
     return (
-      <div className="searchSection">
-
+      <div className="container-fluid searchSection">
+      <h1 class="display-1">Recipe Box</h1>
         <form onSubmit={this.props.fetchRecipes}>
+        <div class="input-group mb-3">
           <input
             value={this.props.term}
             type="text"
+            class="form-control"
             placeholder="search recipe"
+            aria-label="Search Recipe"
+            aria-describedby="basic-addon2"
             onChange={this.props.handleChange}
           />
-        <button type="submit">search</button>
+          <div class="input-group-append">
+            <span class="input-group-text"><button type="submit"><i class="fas fa-search"></i></button></span>
+          </div>
+        </div>
         </form>
       </div>
     )
